016feae737ccae97d1ece6c4bec39b1c904d3fb3
junny <erranteldoni@gmail.com>
2023-03-08 16:25:24 +0000

    search done

--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django import forms
 import markdown
 from . import util
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
     })
 
 def entry(request, title):
@@ -23,5 +25,19 @@ def entry(request, title):
         "page_title": title,
         "page_text": page_text,
     })
+    
+def search(request):
+    q = request.POST.get('q')
+    if q in util.list_entries():
+        return redirect('entry', q)
+    else:
+        partial_matches = []
+        for page in util.list_entries():
+            if q.lower() in page.lower():
+                partial_matches.append(page)
+        return render(request, "encyclopedia/results.html", {
+            "matches": partial_matches,
+        })
+    
     
         
