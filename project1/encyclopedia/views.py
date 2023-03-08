e03ae3a7af41900dbc60d7d0a3f7c54614ae59a8
junny <erranteldoni@gmail.com>
2023-03-08 13:00:08 +0000

    03-08

--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+import markdown
 from . import util
 
 
@@ -8,3 +9,19 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    # Show first links
+    
+    file = util.get_entry(title)
+    if not file:
+        return render(request, "encyclopedia/404.html", {
+            "page_title": 404
+        })
+    page_text = markdown.markdown(file)
+    
+    return render(request, "encyclopedia/entry.html", {
+        "page_title": title,
+        "page_text": page_text,
+    })
+    
+        
