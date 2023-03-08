bdb4d58c1b814ed703313b213ec7cf66c782fd25
junny <erranteldoni@gmail.com>
2023-03-08 10:08:32 +0000

    03-08

--- testing/lecture3/tasks/views.py
+++ testing/lecture3/tasks/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
 
 tasks = ["foo", "bar", "baz"]
 # Create your views here.
@@ -6,3 +12,18 @@ def index(request):
     return render(request, "tasks/index.html", {
         "tasks": tasks,
     })
+    
+def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm,
+    }) 
