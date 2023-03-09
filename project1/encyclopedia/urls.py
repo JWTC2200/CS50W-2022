99398e42f2a4d355ae3c6b8d48cddd40c1af1ed3
junny <erranteldoni@gmail.com>
2023-03-09 18:14:27 +0000

    Edit & random complete

--- project1/encyclopedia/urls.py
+++ project1/encyclopedia/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("wiki/<title>", views.entry, name="entry"),
     path("search", views.search, name="search"),
     path("newpage", views.newpage, name="newpage"),
+    path("edit/<title>", views.edit, name="edit"),
+    path("random", views.random, name="random"),
 ]
--- project1/encyclopedia/views.py
+++ project1/encyclopedia/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django import forms
-import markdown
+from random import choice
+import markdown2
 from . import util
 
 
 class new_page_form(forms.Form):
-    title = forms.CharField(required = True)
-    text = forms.CharField(required = True, widget = forms.Textarea)
+    title = forms.CharField(required = True, widget = forms.TextInput(attrs={"placeholder": "Page title", "style": "width: 300px", "class": "form-control" }))
+    text = forms.CharField(required = True, widget = forms.Textarea(attrs={"placeholder": "Page text", "style": "width: 600px", "class": "form-control", "rows": "20"}))
+    
+class edit_page_form(forms.Form):
+    title = forms.CharField(required = True, widget = forms.TextInput(attrs={"placeholder": "Page title", "style": "width: 300px", "class": "form-control" }))
+    text = forms.CharField(required = True, widget = forms.Textarea(attrs={"placeholder": "Page text", "style": "width: 600px", "class": "form-control", "rows": "20"}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -21,7 +26,7 @@ def entry(request, title):
     file = util.get_entry(title)
     if not file:
         return render(request, "encyclopedia/404.html")
-    page_text = markdown.markdown(file)
+    page_text = markdown2.markdown(file)
     
     return render(request, "encyclopedia/entry.html", {
         "page_title": title,
@@ -59,4 +64,31 @@ def newpage(request):
         "new_page_form": new_page_form
     })
     
+def edit(request, title):  
+    if request.method == "POST":
+        form = edit_page_form(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get("title")
+            text = form.cleaned_data.get("text")
+            text = f'# {title}\n{text}'
+            util.save_entry(title, text)
+            return redirect('entry', title)
+   
+    else:
+        with open(f'./entries/{title}.md') as current_page:
+                current_data = current_page.readlines()   
+                
+        entry_data = {
+            "title": title,
+            "text": " ".join(current_data[1:]),
+            }
         
+        return render(request, "encyclopedia/editpage.html", {
+            "edit_page_form": edit_page_form(entry_data),
+        })
+
+    
+def random(request):
+    random_page = choice(util.list_entries())
+    return redirect('entry', random_page)
+    
