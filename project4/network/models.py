782f3c0cfae38197f053ce28668e087e6be59e71
junny <erranteldoni@gmail.com>
2023-04-18 17:58:09 +0100

    models

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,26 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Posts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=280)
+    datetime = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, symmetrical=False, blank=True, related_name="likes")
+    
+    def LikeCount(self):
+        return self.likes.count()
+    
+    class Meta:
+        ordering = ['-datetime']
+        
+class Follows(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    following = models.ManyToManyField(User, symmetrical=False, blank=True, related_name="following")
+    followedby = models.ManyToManyField(User, symmetrical=False, blank=True, related_name="followedby")
+    
+    def FollowCount(self):
+        return self.following.count()
+    
+    def FollowedByCount(self):
+        return self.followedby.count()
