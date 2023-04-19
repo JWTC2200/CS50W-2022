b54088f8bf79af713156fa15f7fc760a5ebec7a8
junny <erranteldoni@gmail.com>
2023-04-19 12:38:09 +0100

    show posts

--- project4/network/views.py
+++ project4/network/views.py
@@ -3,12 +3,21 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 from .models import User, Posts, Follows
 
 
 def index(request):
-    return render(request, "network/index.html")
+    all_posts = Posts.objects.all()
+    paginator_all = Paginator(all_posts, 3)
+    page_number = 1
+    if request.GET.get("page") != None:
+        page_number = request.GET.get("page")
+    return render(request, "network/index.html", {
+        "all_posts": paginator_all.page(page_number),
+        })
 
 
 def login_view(request):
@@ -61,18 +70,23 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
-    
+
+@login_required
 def user(request):
     if request.method == "POST":
         post_content = request.POST["post-content"]
         if len(post_content) > 0:
             Posts(user=request.user, content=post_content).save()
-        return render(request, "network/user.html")    
-        
-            
     
-    else:
-        return render(request, "network/user.html")
+    username = request.user.username
+    user = User.objects.get(username=username)
+    user_posts = Posts.objects.filter(user=user)
+    
 
+    return render(request, "network/user.html", {
+        "user_posts": user_posts,
+    })    
+        
+@login_required
 def following(request):
     return render(request, "network/following.html")
