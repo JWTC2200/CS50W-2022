0dc9a8b99bb660d3f2feed238012b8a92df9d115
junny <erranteldoni@gmail.com>
2023-04-23 15:50:33 +0100

    js

---
+++ capstone/calculator/static/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', fucntion() {
+
+})
+
+
+function show_block() {
+    console.log(document.querySelector('#artifact-stat-block'))
+    document.querySelector('#artifact-stat-block').hidden = false;
+}
